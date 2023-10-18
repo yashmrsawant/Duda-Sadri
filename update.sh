1c90fe36944d30ff2896982494da90890fb4a818
Yash M Sawant <yashmrsawant@gmail.com>
2023-10-18 16:55:13 +0530

    Updating

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 
 # Provide the username and password for authentication
 #username = "yashmrsawant"
-#password = "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"
+#password = "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"
 
 git config credential.helper store
 git config --global credential.username yashmrsawant
@@ -12,7 +12,7 @@ git credential approve <<EOF
 protocol=https
 host=github.com
 username=yashmrsawant
-password=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
+password=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
 EOF
 
 # Update the repository with the provided credentials
@@ -26,7 +26,7 @@ git credential reject <<EOF
 protocol=https
 host=github.com
 username=yashmrsawant
-password=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
+password=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
 EOF
 
 echo "Repository update complete!"
