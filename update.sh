728e8e4598f597df132268e84dd424891c6f152c
Yash M Sawant <yashmrsawant@gmail.com>
2023-10-18 17:02:52 +0530

    Updating

--- update.sh
+++ update.sh
@@ -4,29 +4,20 @@
 
 # Provide the username and password for authentication
 #username = "yashmrsawant"
-#password = "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"
+#password = "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"
 
-git config credential.helper store
-git config --global credential.username yashmrsawant
+#git config credential.helper store
+#git config --global credential.username yashmrsawant
 git credential approve <<EOF
 protocol=https
 host=github.com
 username=yashmrsawant
-password=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
+password=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
 EOF
 
 # Update the repository with the provided credentials
 git add --a 
 git commit -m "Updating"
 git push origin main
-
-# Clear the stored credentials
-git config --unset-all credential.username
-git credential reject <<EOF
-protocol=https
-host=github.com
-username=yashmrsawant
-password=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
-EOF
 
 echo "Repository update complete!"
